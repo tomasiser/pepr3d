df76e01c7fb2dc0ec09e68da05639a7126fd83df
Tomáš Iser <thomosik@gmail.com>
2018-08-01 22:08:39 +0200

    Testing circleci

--- src/Pepr3D.cpp
+++ src/Pepr3D.cpp
@@ -35,7 +35,7 @@ void Pepr3D::mouseDown(MouseEvent event) {}
 void Pepr3D::fileDrop(FileDropEvent event) {
     mDroppedFiles = "";
     for(auto path : event.getFiles()) {
-        mDroppedFiles += path.u8string();
+        mDroppedFiles += path.string();
         mDroppedFiles += '\n';
         mFilesAlreadyDropped = true;
     }
