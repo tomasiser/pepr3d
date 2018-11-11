938054b573a9f38fff9b6e2299e476775a811da2
Tomáš Iser <thomosik@gmail.com>
2018-11-11 21:26:44 +0100

    Fix unit tests for new default colors and geometry (cube)

--- src/geometry/Geometry.test.cpp
+++ src/geometry/Geometry.test.cpp
@@ -6,16 +6,16 @@
 
 TEST(Geometry, initialize) {
     pepr3d::Geometry geo;
-    EXPECT_EQ(geo.getTriangleCount(), 2);
+    EXPECT_EQ(geo.getTriangleCount(), 12);
 
     const auto vertexBuffer = geo.getVertexBuffer();
-    EXPECT_EQ(vertexBuffer.size(), 6);
+    EXPECT_EQ(vertexBuffer.size(), 36);
     const auto indexBuffer = geo.getIndexBuffer();
-    EXPECT_EQ(indexBuffer.size(), 6);
+    EXPECT_EQ(indexBuffer.size(), 36);
     const auto normalBuffer = geo.getNormalBuffer();
-    EXPECT_EQ(normalBuffer.size(), 6);
+    EXPECT_EQ(normalBuffer.size(), 36);
     const auto colorBuffer = geo.getColorBuffer();
-    EXPECT_EQ(colorBuffer.size(), 6);
+    EXPECT_EQ(colorBuffer.size(), 36);
 }
 
 TEST(Geometry, getColor) {
@@ -25,10 +25,10 @@ TEST(Geometry, getColor) {
     EXPECT_EQ(geo.getTriangleColor(1), 0);
 
     const auto colorBuffer = geo.getColorBuffer();
-    EXPECT_EQ(colorBuffer.size(), 6);
+    EXPECT_EQ(colorBuffer.size(), 36);
     pepr3d::Geometry::ColorIndex colorIndex = colorBuffer.at(0);
 
-    for(int i = 1; i < 6; ++i) {
+    for(int i = 1; i < 36; ++i) {
         EXPECT_EQ(colorIndex, colorBuffer.at(i));
     }
 }
@@ -40,7 +40,7 @@ TEST(Geometry, setColor) {
     EXPECT_EQ(geo.getTriangleColor(1), 0);
 
     auto& colorBuffer = geo.getColorBuffer();
-    EXPECT_EQ(colorBuffer.size(), 6);
+    EXPECT_EQ(colorBuffer.size(), 36);
     const pepr3d::Geometry::ColorIndex colorIndex = colorBuffer.at(0);
 
     geo.setTriangleColor(1, 3);
@@ -54,6 +54,10 @@ TEST(Geometry, setColor) {
 
     for(int i = 3; i < 6; ++i) {
         EXPECT_EQ(colorBuffer.at(i), newColorIndex);
+    }
+
+    for(int i = 7; i < 36; ++i) {
+        EXPECT_EQ(colorBuffer.at(i), colorIndex);
     }
 }
 #endif
