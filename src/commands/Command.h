6d5ef2cea88d0cc4540eef4d22be720e4cb8060d
Luis Sanchez <Fro-Z@users.noreply.github.com>
2018-11-11 16:26:01 +0100

    Merge pull request #39 from tomasiser/luis/undoRework
    Command joining and better rules for snapshots

--- src/commands/Command.h
+++ src/commands/Command.h
@@ -1,23 +1,54 @@
 #pragma once
 #include <string>
 
-/*
-ICommandBase is a base class for commands representing a single undoable operation.
+namespace pepr3d {
+
+/**
+CommandBase is a base class for commands representing a single undoable operation.
 The command itself should be an immutable entity, containing only the necessary data to perform the
 action.
 */
 template <typename Target>
-class ICommandBase {
+class CommandBase {
     template <typename>
     friend class CommandManager;  // allow only CM to run the commands
    public:
-    ICommandBase() = default;
-    virtual ~ICommandBase() = default;
+    /**
+     * @param isSlow Mark command as slow. CommandManager will create a snapshot after a slow command.
+     * @param commandType Command type is used to determine if two commands can be joined. If commandType is zero no
+     * attempt will be made.
+     */
+    CommandBase(bool isSlow = false, uint64_t commandType = 0) : mIsSlow(isSlow), mCommandType(commandType) {}
+    virtual ~CommandBase() = default;
 
     /// Get description of the command
     virtual std::string_view getDescription() const = 0;
 
+    /// Is command slow and thus snapshot will be created before next command
+    bool isSlowCommand() const {
+        return mIsSlow;
+    }
+
+    /// Get value representing the command's type. Commands of the same type other than zero are assumed to be joinable
+    uint64_t getCommandType() const {
+        return mCommandType;
+    }
+
    protected:
     /// Run the command, applying the modifications to the target
     virtual void run(Target& target) const = 0;
+
+    /**
+     * Join this command with other command of the same commandType, uniting both commands into this command's contents.
+     * @return true on successful join
+     */
+    virtual bool joinCommand(const CommandBase&) {
+        return false;
+    }
+
+   private:
+    const bool mIsSlow = false;
+    const uint64_t mCommandType;
 };
+
+}  // namespace pepr3d
