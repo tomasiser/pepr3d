3c386b24f6f9ddbb4322937dc0133912dde44249
Štěpán Hojdar <hojdar.stepan@gmail.com>
2018-11-19 10:53:46 +0100

    Fixed crash in release, because CGAL does not throw excp in release
    Now we detect the badly loaded polyhedron manually

--- src/geometry/Geometry.h
+++ src/geometry/Geometry.h
@@ -339,6 +339,13 @@ class Geometry {
             return;
         }
 
+        // The exception does not get thrown in Release
+        if(!mPolyhedronData.P.is_valid() || mPolyhedronData.P.is_empty()) {
+            mPolyhedronData.P.clear();
+            CI_LOG_E("Polyhedron loaded empty or invalid.");
+            return;
+        }
+
         assert(mPolyhedronData.P.size_of_facets() == mPolyhedronData.indices.size());
         assert(mPolyhedronData.P.size_of_vertices() == mPolyhedronData.vertices.size());
 
