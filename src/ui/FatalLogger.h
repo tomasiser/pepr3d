3fb80b2b93cac30340760bd96d854727fdfcae95
Tomáš Iser <thomosik@gmail.com>
2019-01-04 21:30:02 +0100

    Better logging

---
+++ src/ui/FatalLogger.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "cinder/Log.h"
+
+namespace pepr3d {
+
+class FatalLogger : public ci::log::LoggerFile {
+   public:
+    FatalLogger(const ci::fs::path& filePath, bool appendToExisting) : LoggerFile(filePath, appendToExisting) {}
+
+    virtual void write(const ci::log::Metadata& meta, const std::string& text) override {
+        if(meta.mLevel != ci::log::Level::LEVEL_FATAL) {
+            return;
+        }
+
+        LoggerFile::write(meta, text);
+    }
+};
+
+};  // namespace pepr3d
