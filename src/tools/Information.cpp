053284383a58284e2ee1c8549afee33d7fd3afae
Tomáš Iser <thomosik@gmail.com>
2019-01-05 14:39:37 +0100

    Text formatting change

--- src/tools/Information.cpp
+++ src/tools/Information.cpp
@@ -5,12 +5,14 @@
 namespace pepr3d {
 void Information::drawToSidePane(SidePane& sidePane) {
     sidePane.drawText(
-        "Pepr3D is a student project made by a group of Charles University students during 2018 and 2019. It is "
-        "designed to be used in the 3D printing pipeline as a way to separate different materials into separate "
+        "Pepr3D is a student project made by a group of Charles University students during the years 2018 and 2019.");
+
+    sidePane.drawText(
+        "It is designed to be used in a 3D printing pipeline as a way to separate different materials into separate "
         "files.");
 
-    sidePane.drawText("This software was made to complete the course NPRG023 - Software project.");
+    sidePane.drawText("This software was made to complete the course NPRG023 - Software Project.");
 
-    sidePane.drawText("You can see and contribute to the code at https://github.com/tomasiser/pepr3d");
+    sidePane.drawText("See and contribute to the code:\nhttps://github.com/tomasiser/pepr3d");
 }
 }  // namespace pepr3d
