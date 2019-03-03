3e9844aa13e615d0a249cf5be411fc1ef29504d9
Fro-Z <fro-z@email.cz>
2019-03-03 14:26:17 +0100

    Brush tooltips

--- src/tools/Brush.cpp
+++ src/tools/Brush.cpp
@@ -91,34 +91,49 @@ void Brush::drawToSidePane(SidePane& sidePane) {
                               140.f);
 
     sidePane.drawIntDragger("Segments", mBrushSettings.segments, 0.1, 3, 50, "%d", 140.f);
+    sidePane.drawTooltipOnHover("Higher number of segments increases \"roundness\" of the brush.");
 
     sidePane.drawCheckbox("Paint backfaces", mBrushSettings.paintBackfaces);
+    sidePane.drawTooltipOnHover("Paint triangles even if they are facing away from the camera.");
 
     sidePane.drawSeparator();
     sidePane.drawText("Brush shape:");
     if(ImGui::RadioButton("Sphere", mBrushSettings.spherical)) {
         mBrushSettings.spherical = true;
     }
+    sidePane.drawTooltipOnHover(
+        "Spherical brush paints everything within a radius, and will create additional edges to smooth transitions "
+        "over triangle boundaries.");
     if(ImGui::RadioButton("Flat", !mBrushSettings.spherical)) {
         mBrushSettings.spherical = false;
         mApplication.getCurrentGeometry()->hideHighlight();
     }
+    sidePane.drawTooltipOnHover(
+        "Flat brush paints the shape directly to the surface, ignoring any distance limitations.");
 
     if(mBrushSettings.spherical) {
         sidePane.drawText("Spherical brush settings");
 
         sidePane.drawCheckbox("Continuous", mBrushSettings.continuous);
+        sidePane.drawTooltipOnHover(
+            "Paint only triangles that are connected inside the painting radius. This prevents accidentaly painting "
+            "two parts connected only via an air-gap.");
 
         sidePane.drawCheckbox("Respect original triangles", mBrushSettings.respectOriginalTriangles);
+        sidePane.drawTooltipOnHover(
+            "Prevent brush from creating new triangles. This makes Brush tool behave like a Triangle Painter tool with "
+            "a radius.");
 
         if(mBrushSettings.respectOriginalTriangles) {
             sidePane.drawCheckbox("Paint outer ring", mBrushSettings.paintOuterRing);
+            sidePane.drawTooltipOnHover("Paint the whole triangle, even if it is not fully inside the brush.");
         }
     }
 
     if(!mBrushSettings.spherical) {
         sidePane.drawText("Flat brush settings");
         sidePane.drawCheckbox("Align to normal", mBrushSettings.alignToNormal);
+        sidePane.drawTooltipOnHover("Align painting direction to the surface normal.");
     }
     sidePane.drawSeparator();
     mPaintsSinceDraw = 0;
